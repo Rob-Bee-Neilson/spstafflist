823d4021bf29432a504002248aab029a180e2dd8
rob-bee-neilson <robbie.neilson@gmail.com>
2019-09-25 22:39:16 +1200

    test list displaying in App

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 <!-- Given a file (`staff_list.json` in this directory), containing a JSON encoded list of Staff, create an program to read and parse this file to display all staff, ordered by name, who live within 2km of our office, which is located at 175 Victoria Street, Wellington (lat: -41.2920728, long: 174.7748162). -->
 
+requirements:
+access data from staff_list.json
+remove titles in names
+sort data by name
+filter data by location
+render list of names
+
+
 1 set up environment
 2 install npm geolib
 access data from staff_list.json
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
+import { getDistance } from 'geolib';
 
+const geolib = require('geolib');
 const Data = require('../../server/public/staff_list.json')
 
+// staffInfo is 1 object whoes value is an array of objects
 let staffInfo = Object.values(Data)[0]
+// staffList is 1 array containing objects
 let staffList = staffInfo.map(foundStaff=>{
-  return foundStaff.name
- })
+  return foundStaff.name + foundStaff.location.latitude
+ }).sort()
+let locationList = staffInfo.map(range=>{
+  return range.location
+ }).forEach(function(element) {
+  console.log(element);
+});
 
-console.log(staffList)
+// getDistance({latitude: -41.2920728, longitude: 174.7748162}, range.location)
+// console.log(staffList.location)
+console.log(locationList)
+
+// getDistance(
+//   { latitude: 51.5103, longitude: 7.49347 },
+//   { latitude: "51° 31' N", longitude: "7° 28' E" }
+// )
+
+// console.log(getDistance(
+//   { latitude: 51.5103, longitude: 7.49347 },
+//   { latitude: "51° 31' N", longitude: "7° 28' E" }
+// ))
+
+function list(arr) {
+  arr.forEach(function(element){
+    console.log(element);
+    return "hi"
+  })
+}
 
 
 const App = () => {
   return (
+    <div>
     <h1>Staff Lister</h1>
+
+      <p>Here are the peeps:<br></br></p>
+    
+      <ul>
+        {staffInfo.map(foundStaff=>{
+        return <li key={foundStaff.id}>{foundStaff.name + foundStaff.location.latitude}</li>
+        })}
+      </ul>
+
+    </div>
   )
 }
 
