94832732ce76a6a2bf980942b640c0b838d4d7d6
rob-bee-neilson <robbie.neilson@gmail.com>
2019-09-26 15:22:11 +1200

    AllStaff companent created

---
+++ client/components/AllStaff.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const data = require('../../server/public/staff_list.json')
+const staffInfo = Object.values(data)[0]
+
+let allStaff = staffInfo.map(staff => {return staff.name}).sort();
+
+const AllStaff = () => {
+    console.log(allStaff)
+    return (
+        <React.Fragment>
+            <ul>
+                {allStaff.map(show=>{
+                    return <li key={show}>{show}</li>
+                })}
+            </ul>
+        </React.Fragment>
+    )
+}
+export default AllStaff
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,51 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { getDistance } from 'geolib';
+import AllStaff from './AllStaff'
 
-// const geolib = require('geolib');
 const Data = require('../../server/public/staff_list.json')
-
-// staffInfo is 1 array of objects
-let staffInfo = Object.values(Data)[0]
-
-// staffList is 1 array containing objects
-let staffList = staffInfo.map(foundStaff=>{
-  return foundStaff.name + foundStaff.location.latitude
- }).sort()
+const staffInfo = Object.values(Data)[0]
 
 let locationList = staffInfo.filter(inRange=>{
   if (getDistance({latitude: -41.2920728, longitude: 174.7748162}, inRange.location) < 2000) {
     return inRange
   }
 }).map(staff => {return staff}).sort();
-// ).forEach(function(element) {
-//   if (getDistance({latitude: -41.2920728, longitude: 174.7748162}, element) < 2000) {
-//     console.log(range.location.name);
-//   }
-// });
-  //  }).forEach(function(element) {
-//   console.log(element);
-// });
 
-// getDistance({latitude: -41.2920728, longitude: 174.7748162}, range.location)
-// console.log(staffList.location)
 console.log(locationList)
-
-// getDistance(
-//   { latitude: 51.5103, longitude: 7.49347 },
-//   { latitude: "51° 31' N", longitude: "7° 28' E" }
-// )
-
-// console.log(getDistance(
-//   { latitude: 51.5103, longitude: 7.49347 },
-//   { latitude: "51° 31' N", longitude: "7° 28' E" }
-// ))
-
-function list(arr) {
-  arr.forEach(function(element){
-    console.log(element);
-    return "hi"
-  })
-}
 
 
 const App = () => {
@@ -57,16 +23,11 @@ const App = () => {
 
       <ul>
         {locationList.map(foundStaff=>{
-          console.log(foundStaff)
           return <li key={foundStaff.id}>{foundStaff.name}</li>
         })}
       </ul>
 
-      <ul>
-        {staffInfo.map(foundStaff=>{
-        return <li key={foundStaff.id}>{foundStaff.name + foundStaff.location.latitude}</li>
-        })}
-      </ul>
+      <AllStaff />
 
     </div>
   )
